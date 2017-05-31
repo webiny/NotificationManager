3a18096f3d330f6d04b627870c62a93942230208
Pavel910 <pavel@webiny.com>
2017-05-31 11:08:33 +0200

    Template module update

--- Js/Backend/Modules/Templates/Views/TemplateForm.jsx
+++ Js/Backend/Modules/Templates/Views/TemplateForm.jsx
@@ -9,9 +9,7 @@ class TemplateForm extends Webiny.Ui.View {
     }
 
     previewTemplate(content) {
-        const myWindow = window.open('data:text/html,' + encodeURIComponent(content),
-            '_blank', 'width=800,height=600');
-        myWindow.focus();
+        window.open('data:text/html,' + encodeURIComponent(content), '_blank', 'width=800,height=600').focus();
     }
 }
 
@@ -25,56 +23,45 @@ TemplateForm.defaultProps = {
             onCancel: () => Webiny.Router.goToRoute('NotificationManager.Templates')
         };
 
+        const {Ui} = this.props;
+
         return (
             <Ui.Form ui="templateForm" {...formProps}>
                 {(model, container) => (
                     <Ui.View.Form>
                         <Ui.View.Header title="Template"/>
-
                         <Ui.View.Body>
                             <Ui.Grid.Row>
                                 <Ui.Grid.Col all={8}>
-
-                                    <Ui.Form.Section title="Template"/>
-
+                                    <Ui.Section title="Template"/>
                                     <Ui.Input label="Name" name="name" validate="required"/>
                                     <Ui.CodeEditor label="Content" name="content" description="Enter plain text or HTML content"/>
-
                                 </Ui.Grid.Col>
-
                                 <Ui.Grid.Col all={4}>
-
-                                    <Ui.Form.Section title="System Tags"/>
-
+                                    <Ui.Section title="System Tags"/>
                                     <dl>
                                         <dt>&#123;_hostName_&#125;</dt>
                                         <dd>Website address,&nbsp;
                                             <strong>{window.location.origin}</strong>
                                         </dd>
                                     </dl>
-
                                     <dl>
                                         <dt>&#123;_content_&#125;</dt>
                                         <dd>Variable containing notification content.</dd>
                                     </dl>
-
                                     <dl>
                                         <dt>&#123;_subject_&#125;</dt>
                                         <dd>Variable containing notification subject.</dd>
                                     </dl>
-
                                     <dl>
                                         <dt>Notification variables</dt>
                                         <dd>Additionally you can include any of the variables that are available inside your
                                             notification, that will be using this template.
                                         </dd>
                                     </dl>
-
                                 </Ui.Grid.Col>
-
                             </Ui.Grid.Row>
                         </Ui.View.Body>
-
                         <Ui.View.Footer>
                             <Ui.Button align="right" type="primary" onClick={container.submit}>Save Changes</Ui.Button>
                             <Ui.Button align="right" type="secondary" onClick={() => this.previewTemplate(model.content)}>Preview Template</Ui.Button>
@@ -88,4 +75,7 @@ TemplateForm.defaultProps = {
 };
 
 
-export default TemplateForm;
+export default Webiny.createComponent(TemplateForm, {
+    modulesProp: 'Ui',
+    modules: ['Form', 'View', 'Grid', 'Section', 'Input', 'CodeEditor', 'Button']
+});
--- Js/Backend/Modules/Templates/Views/TemplateList.jsx
+++ Js/Backend/Modules/Templates/Views/TemplateList.jsx
@@ -21,48 +21,47 @@ TemplateList.defaultProps = {
             name: '_searchQuery'
         };
 
+        const {View, Grid, Link, Icon, List, Input, Button} = this.props;
+
         return (
-            <Webiny.Ui.LazyLoad modules={['View', 'Grid', 'Link', 'Icon', 'List', 'Input', 'Button']}>
-                {({View, Grid, Link, Icon, List, Input, Button}) => (
-                    <View.List>
-                        <View.Header title="Templates">
-                            <Link type="primary" align="right" route="NotificationManager.Template.Create">
-                                <Icon icon="icon-plus-circled"/>
-                                Create new Template
-                            </Link>
-                        </View.Header>
-                        <View.Body>
-                            <List ui="templateList" {...listProps}>
-                                <List.FormFilters>
-                                    {(applyFilters, resetFilters) => (
-                                        <Grid.Row>
-                                            <Grid.Col all={10}>
-                                                <Input {...searchProps} onEnter={applyFilters()}/>
-                                            </Grid.Col>
-                                            <Grid.Col all={2}>
-                                                <Button type="secondary" align="right" label="Reset Filter" onClick={resetFilters()}/>
-                                            </Grid.Col>
-                                        </Grid.Row>
-                                    )}
-                                </List.FormFilters>
-                                <List.Table>
-                                    <List.Table.Row>
-                                        <List.Table.Field name="name" align="left" label="Name" sort="name" route="NotificationManager.Template.Edit"/>
-                                        <List.Table.TimeAgoField name="createdOn" align="left" label="Created" sort="createdOn"/>
-                                        <List.Table.Actions>
-                                            <List.Table.EditAction route="NotificationManager.Template.Edit"/>
-                                            <List.Table.DeleteAction/>
-                                        </List.Table.Actions>
-                                    </List.Table.Row>
-                                </List.Table>
-                                <List.Pagination/>
-                            </List>
-                        </View.Body>
-                    </View.List>
-                )}
-            </Webiny.Ui.LazyLoad>
+            <View.List>
+                <View.Header title="Templates">
+                    <Link type="primary" align="right" route="NotificationManager.Template.Create">
+                        <Icon icon="icon-plus-circled"/>
+                        Create new Template
+                    </Link>
+                </View.Header>
+                <View.Body>
+                    <List ui="templateList" {...listProps}>
+                        <List.FormFilters>
+                            {(applyFilters, resetFilters) => (
+                                <Grid.Row>
+                                    <Grid.Col all={10}>
+                                        <Input {...searchProps} onEnter={applyFilters()}/>
+                                    </Grid.Col>
+                                    <Grid.Col all={2}>
+                                        <Button type="secondary" align="right" label="Reset Filter" onClick={resetFilters()}/>
+                                    </Grid.Col>
+                                </Grid.Row>
+                            )}
+                        </List.FormFilters>
+                        <List.Table>
+                            <List.Table.Row>
+                                <List.Table.Field name="name" align="left" label="Name" sort="name"
+                                                  route="NotificationManager.Template.Edit"/>
+                                <List.Table.TimeAgoField name="createdOn" align="left" label="Created" sort="createdOn"/>
+                                <List.Table.Actions>
+                                    <List.Table.EditAction route="NotificationManager.Template.Edit"/>
+                                    <List.Table.DeleteAction/>
+                                </List.Table.Actions>
+                            </List.Table.Row>
+                        </List.Table>
+                        <List.Pagination/>
+                    </List>
+                </View.Body>
+            </View.List>
         );
     }
 };
 
-export default TemplateList;
+export default Webiny.createComponent(TemplateList, {modules: ['View', 'Grid', 'Link', 'Icon', 'List', 'Input', 'Button']});
