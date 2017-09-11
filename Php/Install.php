5d778f981d7bd8a3a1d913d883d65f4ad1411ac3
Pavel910 <pavel@webiny.com>
2017-09-11 19:30:27 +0200

    Install class is simplified to only getter methods.

--- Php/Install.php
+++ Php/Install.php
@@ -2,20 +2,15 @@
 
 namespace Apps\NotificationManager\Php;
 
-use Apps\Webiny\Php\Lib\Apps\App;
-
 class Install extends \Apps\Webiny\Php\Lib\LifeCycle\Install
 {
-    public function run(App $app)
+    public function getUserPermissions()
     {
-        parent::run($app);
-        
-        // Insert permissions
-        $permissions = json_decode(file_get_contents(__DIR__ . '/Install/UserPermissions.json'), true);
-        $this->createUserPermissions($permissions);
+        return json_decode(file_get_contents(__DIR__ . '/Install/UserPermissions.json'), true);
+    }
 
-        // Insert roles
-        $roles = json_decode(file_get_contents(__DIR__ . '/Install/UserRoles.json'), true);
-        $this->createUserRoles($roles);
+    public function getUserRoles()
+    {
+        return json_decode(file_get_contents(__DIR__ . '/Install/UserRoles.json'), true);
     }
 }
