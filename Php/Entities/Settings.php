414192a5b5c0c2f14f4c638d76c70a1f9a145400
Pavel910 <pavel@webiny.com>
2017-09-28 03:26:05 +0200

    Updates on notifications and settings classes.

--- Php/Entities/Settings.php
+++ Php/Entities/Settings.php
@@ -5,7 +5,7 @@ namespace Apps\NotificationManager\Php\Entities;
 /**
  * Class Settings
  */
-class Settings extends \Apps\Webiny\Php\Entities\Settings
+class Settings extends \Apps\Webiny\Php\Entities\AbstractSettings
 {
     protected static $classId = 'NotificationManager.Entities.Settings';
     protected static $key = 'notification-manager';
