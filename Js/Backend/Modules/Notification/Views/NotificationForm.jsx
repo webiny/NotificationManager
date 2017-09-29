093a35d118053e4d0bddf17dee737ad00e205d66
adrian.1358@gmail.com <4ndr055!>
2017-09-29 08:14:25 +0200

    Console log removed.

--- Js/Backend/Modules/Notification/Views/NotificationForm.jsx
+++ Js/Backend/Modules/Notification/Views/NotificationForm.jsx
@@ -86,7 +86,6 @@ NotificationForm.defaultProps = {
             <Form {...formProps}>
                 {({model, form}) => (
                     <View.Form>
-                        {console.log(model)}
                         <View.Header title={this.i18n('Notification')}/>
                         <View.Body noPadding>
                             <PreviewModal ref={ref => this.previewModal = ref} model={model} form={form}/>
