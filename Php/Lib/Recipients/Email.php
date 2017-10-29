e7ef11c9266fae1fb23898f4f30ace03365c5bad
sven <sven@webiny.com>
2017-10-29 12:53:50 +0000

    [Improvement] Email class now implements RecipientInterface.

--- Php/Lib/Recipients/Email.php
+++ Php/Lib/Recipients/Email.php
@@ -2,7 +2,16 @@
 
 namespace Apps\NotificationManager\Php\Lib\Recipients;
 
-class Email extends \Webiny\Component\Mailer\Email
+class Email extends \Webiny\Component\Mailer\Email implements RecipientInterface
 {
 
+    /**
+     * Returns a unique string identifying this recipient.
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->email;
+    }
 }
